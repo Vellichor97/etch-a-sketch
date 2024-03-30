2608315c755e10f4a5131161835c40284908f9f5
Vellichor97 <developer.andy97@gmail.com>
2024-03-30 19:53:35 +0200

    add coloring logic - make it so the person does not have to click on every cell but can hold the mouse down and just move it

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 
-
+// To change these values so they can be picked by user
 let size = 16;
+let currentColor = 'black';
 
 const containerHeight = 500;
 const containerWidth = 450;
@@ -12,16 +13,34 @@ const sketchContainer = document.querySelector('.sketch-container');
 sketchContainer.style.minHeight = containerHeight + 'px';
 sketchContainer.style.minWidth = containerWidth + 'px';
 
+let mouseDown = 0;
+
+document.body.addEventListener('mousedown', () => mouseDown++);
+document.body.addEventListener('mouseup', () => mouseDown--);
+
+
+function enterCell(ev) {
+    if (mouseDown)
+        ev.target.style.backgroundColor = currentColor;
+}
+
+
+function colorCell(ev) {
+    ev.target.style.backgroundColor = 'black';
+}
+
 for (let i = 0; i < size; i++) {
     const row = document.createElement('div');
     row.classList.add('row-container');
     row.style.minHeight = rowDimension + 'px';
-    // alert(row.style.minHeight);
 
     for (let j = 0; j < size; j++) {
         const cell = document.createElement('div');
         cell.classList.add('cell');
         cell.style.minWidth = cellDimension + 'px';
+        
+        cell.addEventListener('mouseenter', enterCell);
+        cell.addEventListener('mousedown', colorCell);
 
         row.appendChild(cell);
     }
